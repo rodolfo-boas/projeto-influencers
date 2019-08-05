a711a68b49feadb23f7c7a1a769ecd2833b978c4
juliano95 <julliano.o95@gmail.com>
2019-08-05 18:47:31 -0300

    header-semi

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,22 @@
+<!-- <ul class="navbar-nav d-flex justify-content-md-end"> -->
+<!-- <ul>
+                    <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/"
+                            target="_blank">Facebook</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://twitter.com/" target="_blank">Twitter</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/?hl=pt-br"
+                            target="_blank">Instagram</a></li>
+                    <li class="nav-item"><a class="nav-link" href=" contato.php">Entre em cotato atraves do
+                            e-mail</a></li>
+                </ul> -->
+
 <footer class="page-footer font-small blue fixarRodape">
     <div class="container-fluid text-center">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 textFooter">
                 <h5 class="text-uppercase">REACH ME OUT</a><span class="dot-color">.</h5>
             </div>
-            <div class="col-12 col-md-12 footer-copyright text-center">© 2019 Copyright: 
+            <div class="col-12 col-md-12 footer-copyright text-center">© 2019 Copyright:
                 <a href="index.php">reachmeout.com</a>
             </div>
         </div>
