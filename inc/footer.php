d96034d6e30bb7ba9741957e5f1fcb185ab86f79
juliano95 <julliano.o95@gmail.com>
2019-08-13 19:53:12 -0300

    Barra-menu

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,4 @@
-<!-- <ul class="navbar-nav d-flex justify-content-md-end"> -->
-<!-- <ul>
-                    <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/"
-                            target="_blank">Facebook</a></li>
-                    <li class="nav-item"><a class="nav-link" href="https://twitter.com/" target="_blank">Twitter</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/?hl=pt-br"
-                            target="_blank">Instagram</a></li>
-                    <li class="nav-item"><a class="nav-link" href=" contato.php">Entre em cotato atraves do
-                            e-mail</a></li>
-                </ul> -->
-
-<footer class="page-footer font-small blue fixarRodape">
+<!-- <footer class="page-footer font-small blue fixarRodape">
     <div class="container-fluid text-center">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 textFooter">
@@ -21,4 +9,52 @@
             </div>
         </div>
     </div>
+</footer> -->
+
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fab fa-linkedin-in"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fab fa-dribbble"> </i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
 </footer>
+<!-- Footer -->
